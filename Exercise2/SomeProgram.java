e2d8d30d22de6a15a3c5962b5b6d9aaf3f599f26
Julia <juliablaumann@gmail.com>
2025-01-20 16:34:50 +0100

    Add first implementation of the program

---
+++ Exercise2/SomeProgram.java
@@ -0,0 +1,10 @@
+# public:  klasa SomeProgram jest dostępny z dowolnego miejsca w programie.
+public class SomeProgram {
+#void: metoda nie zwraca żadnej wartości
+# main: metoda, która jest punktem początkowym każdego programu w Javie.
+# Stringp[] args: Parametr metody, który pozwala przekazywać argumenty wiersza poleceń do programu w formie tabilcy ciągów znaków (tekstów)
+	public static void main(String[] args) {
+# System.out: standardowy output
+		System.out.printIn("We are learning to use Git);
+	}
+}
